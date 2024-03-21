ce1343adcd1f526b6ab1b035722b8938047d18b5
Fabis-li <fabiano_slima@hotmail.com>
2024-03-21 15:17:58 -0300

    Add project files.

---
+++ DiamanteX.ConsoleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace DiamanteX.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diamante de X");
+
+            decimal numero = ObterNumetroInteiro("Digite um número impar");
+
+            decimal qtdeLinhas = (numero / 2) - 0.5M;
+
+            int contador = 0;            
+
+            while(numero % 2 == 0)
+            {
+                Console.WriteLine("Digite um número ímpar");
+                numero = Convert.ToInt32(Console.ReadLine());                
+            }
+
+            int espaco = Convert.ToInt32(qtdeLinhas);
+
+            decimal aux = qtdeLinhas;
+            for (int i = 0; i <= aux; i++)
+            {
+                for(int j = 0;  j <= espaco; j++)
+                {                    
+                    Console.Write(" ");                    
+                }
+
+                for(int k = 0; k <= contador; k++)
+                {
+                    Console.Write("x");                    
+                }
+
+                contador += 2;
+                Console.Write("\n");
+                espaco--;                
+            }
+
+            int novoContador = 2;
+            contador--;
+            for( int i = 0; i<=aux; i++)
+            {
+                for(int j = 0; j < novoContador; j++)
+                {
+                    Console.Write(" ");
+                }
+                novoContador++;
+
+                for(int k = 0; k < )
+                
+            }
+            
+            
+            
+
+
+
+
+
+
+        
+        }
+
+        static decimal ObterNumetroInteiro(string texto)
+        {
+            Console.WriteLine(texto);
+            decimal numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            return numeroDigitado;
+
+        }
+        
+    }
+}
